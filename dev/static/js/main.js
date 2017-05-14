0b02209ec62a4fa3902cb6b9908bd45b224dccca
Ilya Skryabin <808lorem@gmail.com>
2017-05-14 18:48:10 +0300

    Доработал верхний баннер

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -3,8 +3,18 @@
 $(document).ready(function(){
 	// установим обработчик события resize
 	$(window).resize(function(){
-		var h_w = $(window).height();
-		$('.top-banner__slider-block').css('height', h_w);
+		var h_w = $(window).height(),
+			pb_header = parseFloat( $('.header').css('padding-bottom') );
+			h_header = $('.header').height() + pb_header,
+			h_toBannerFlexbox = $('.top-banner__slider_flexbox').height();
+  console.log(pb_header);
+  
+		if(h_w < (h_header + h_toBannerFlexbox)) {
+			h_w = h_header + h_toBannerFlexbox
+		}
+		$('.top-banner__slider_bg').css('height', h_w);
+		$('.top-banner__arrow').css('top', h_w/2);
+		$('.top-banner__slider_absolute').css('top', h_w/2);
 	});
 
 	// вызовем событие resize
@@ -21,7 +31,22 @@ $(document).ready(function(){
 			}
 		});
 	});
-	
+
+	$('.js-slick__top-banner').slick({
+		dots: false,
+		arrows: true,
+		prevArrow: '.top-banner__arrow_prev',
+		nextArrow: '.top-banner__arrow_next',
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+//		responsive: [{
+//			breakpoint: 1500,
+//			settings: {
+//				
+//    		}
+//    	}]
+	});
 	
 	$('.js-slick__featuredwork').slick({
 		dots: true,
@@ -34,12 +59,4 @@ $(document).ready(function(){
 		slidesToScroll: 1,
 	});
 	
-	$('.js-slick__top-banner').slick({
-		dots: false,
-		prevArrow: '.top-banner__arrow_prev',
-		nextArrow: '.top-banner__arrow_next',
-		speed: 300,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-	});
 });
