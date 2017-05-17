2947f96903bacd359a679d7e04b04c6d8c59a50c
Ilya Skryabin <808lorem@gmail.com>
2017-05-17 16:24:51 +0300

    Прочие доработки

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,15 +1,17 @@
 ;
 // Начинать писать отсюда!!!!
+
+var heightHeader = $('.header').height(),
+	paddingTopHeader = parseFloat( $('.header').css('padding-top') ),
+	paddingBottomHeader = parseFloat( $('.header').css('padding-bottom') );
+heightHeader += paddingTopHeader + paddingBottomHeader;
+
 $(document).ready(function(){
 	
 	$('.header__btn-menu').click(function() {
 		var w_w = $(window).width();
 		$('.header__nav').toggleClass('active');
 	});
-	
-	$('.header__item').click(function() {
-		$('.header__nav').removeClass('active');
-	})
 	
 	// установим обработчик события resize
 	$(window).resize(function(){
@@ -35,10 +37,6 @@ $(document).ready(function(){
 	$(window).resize();
 	
 	$('.scroll-link').click(function() {
-		var heightHeader = $('.header').height(),
-			paddingTopHeader = parseFloat( $('.header').css('padding-top') ),
-			paddingBottomHeader = parseFloat( $('.header').css('padding-bottom') );
-		heightHeader += paddingTopHeader + paddingBottomHeader;
 		
 	  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 		var target = $(this.hash);
@@ -60,6 +58,48 @@ $(document).ready(function(){
 			else if ($(this).scrollTop() < 1){
 				$('.header').removeClass('fixed');
 			}
+		});
+	});
+	
+	var menu_selector = ".header__nav"; // Переменная должна содержать название класса или идентификатора, обертки нашего меню.
+ 
+	function onScroll(){
+		var scroll_top = $(document).scrollTop();
+		$(menu_selector + " a").each(function(){
+			var hash = $(this).attr("href");
+			if (hash.charAt(0) !== '#') {
+				return true;
+			} else {
+				var target = $(hash);
+				if (target.position().top <= scroll_top + heightHeader && target.position().top + target.outerHeight() > scroll_top + heightHeader) {
+					$(menu_selector + " a.active").removeClass("active");
+					$(this).addClass("active");
+				} else {
+					$(this).removeClass("active");
+				}
+			}
+		});
+	}
+
+	$(document).ready(function () {
+
+		$(document).on("scroll", onScroll);
+
+		$("a[href^=#]").click(function(e){
+			e.preventDefault();
+
+			$(document).off("scroll");
+			$(menu_selector + " a.active").removeClass("active");
+			$(this).addClass("active");
+			var hash = $(this).attr("href");
+			var target = $(hash);
+
+			$("html, body").animate({
+				scrollTop: target.offset().top
+			}, 500, function(){
+				window.location.hash = hash;
+				$(document).on("scroll", onScroll);
+			});
 		});
 	});
 
@@ -131,3 +171,24 @@ $(document).ready(function(){
     	}]
 	});
 });
+
+
+function range(rangeId, inputId, value, min, max, step, firstText, lastText) {
+	$( function() {
+		$(rangeId).slider({
+			range: "min",
+			value: value,
+			min: min,
+			max: max,
+			step: step,
+			slide: function( event, ui ) {
+				$( inputId ).val(firstText + ui.value + lastText);
+			}
+		});
+		
+		$( inputId ).val(firstText + $(rangeId).slider( "value" ) + lastText);
+	} );
+}
+
+range('#range-price', '#price', 5, 1, 42, 1, '$', ',000');
+range('#range-time', '#time', 2, 1, 10, 1, '', ' weeks');
