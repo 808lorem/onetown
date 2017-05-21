2fe470d9a8a3d02e04d7a052c200639230262c02
Ilya Skryabin <808lorem@gmail.com>
2017-05-22 00:15:32 +0300

    Добавил фильтр для OUR WORKS

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -197,6 +197,26 @@ function range(rangeId, inputId, value, min, max, step, firstText, lastText) {
 		
 		$( inputId ).val(firstText + $(rangeId).slider( "value" ) + lastText);
 	} );
+	
+	
+	$(function() {
+		var newSelection = "";
+
+		$("#flavor-nav a").click(function(){
+
+			$("#all-flavors").fadeTo(200, 0.10);
+
+			$("#flavor-nav a").removeClass("current");
+			$(this).addClass("current");
+
+			newSelection = $(this).attr("rel");
+
+			$(".flavor").not("."+newSelection).slideUp();
+			$("."+newSelection).slideDown();
+
+			$("#all-flavors").fadeTo(600, 1);
+		});
+	});
 }
 
 range('#range-price', '#price', 5, 1, 42, 1, '$', ',000');
